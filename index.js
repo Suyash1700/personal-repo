01dd966876ea9e54c766501c65f2d60934868ac3
Suyash Khade <suyukhade@gmail.com>
2025-05-24 21:42:38 -0700

    Update data for 2025-05-24T21:42:38-07:00

--- index.js
+++ index.js
@@ -10,6 +10,35 @@ const data = {
   date: date,
 };
 
-jsonfile.writeFile(path, data, ()=> {
+const git = simpleGit();
+
+// jsonfile.writeFile(path, data, ()=> {
+//     console.log('Data written to file:', data);
+//     simpleGit().add(['./index.json','./data.json']).commit(date, { '--date': date }).push();});
+
+jsonfile.writeFile(path, data, async err => {
+    if (err) {
+      return console.error('Failed to write JSON:', err);
+    }
+  
     console.log('Data written to file:', data);
-    simpleGit().add('.').commit(date, { '--date': date }).push();});
+  
+    try {
+      // stage your files
+      await git.add(['index.js','data.json']);
+  
+      // commit with your custom date
+      await git.commit(
+        `Update data for ${date}`,      // commit message
+        undefined,                      // files is undefined because we already ran add()
+        { '--date': date }              // commit options
+      );
+  
+      // push to main
+      await git.push('origin', 'main');
+  
+      console.log('Changes committed and pushed!');
+    } catch (e) {
+      console.error('Git error:', e);
+    }
+  });
